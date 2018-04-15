eebaaaaab9f61ab2c10f9e594cff4a081ab0813c
derek chen <derek.g.chen@gmail.com>
2018-04-15 16:52:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -23,22 +23,22 @@ input.addEventListener('keydown', function(e) {
 
 
 }
-// 
+//
 // const alphabet = [65, 66, 67];
-// 
+//
 // // Keep track of index outside of the event handler.
 // let index = 0;
-// 
+//
 // // This is the function that would be invoked by the event listener.
 // function onKeyDownHandler(e) {
 //   const key = parseInt(e.detail || e.which);
-// 
+//
 //   if (key === alphabet[index]) {
 //     index++;
-// 
+//
 //     if (index === alphabet.length) {
 //       alert("Hurray!");
-// 
+//
 //       index = 0;
 //     }
 //   } else {
