a2a3f5c2c0307c9900e669c3d23ea042cba97189
derek chen <derek.g.chen@gmail.com>
2018-04-15 17:07:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,7 +7,7 @@ function init() {
   document.body.onkeydown = function(e){
       alert(String.fromCharCode(e.keyCode)+" --> "+e.keyCode);
       console.log(e.which);
-  }
+  };
   // const key = parseInt(e.detail || e.which);
   //
   // if (key === code[index]) {
