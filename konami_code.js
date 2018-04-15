5b6afdd7ae41be595a2f8aafb8f0c1b2addfb41e
derek chen <derek.g.chen@gmail.com>
2018-04-15 17:03:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,10 +3,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
-  const input = document.querySelector('input')
 
-input.addEventListener('keydown', function(e) {
-  console.log(e.which)
+  document.body.onkeydown = function(e){
+      alert(String.fromCharCode(e.keyCode)+" --> "+e.keyCode);
+  };
   // const key = parseInt(e.detail || e.which);
   // 
   // if (key === code[index]) {
